27623479f828800af630573b6727a6729149aee3
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2022-05-11 18:09:48 +0200

    added jump feature for player and game manager gameobject, changed some physic properties

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalPoint;
-    private float powerupStrength = 15.0f;
+    private GameObject playerObj = null;
+    private float powerupStrength = 800.0f;
 
     public float speed = 2.0f;
+    public float gravityModifier;
+    public float jumpForce;
+
     public bool hasPowerup;
+    public bool gameOver = false;
+    public bool isOnGround = true;
+
     public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerObj = GameObject.FindGameObjectWithTag("Player");
         playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
@@ -25,6 +34,21 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+
+        // jump
+        if (Input.GetKeyDown(KeyCode.Space) && !gameOver && (isOnGround == true))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+
+        // Code for restart
+        //if (playerObj.transform.position.y < -10)
+        //{
+        //    gameOver = true;
+        //    Debug.Log("Game Over!");
+        //    FindObjectOfType<GameManager>().EndGame();
+        //}
     }
 
     private void OnTriggerEnter(Collider other)
@@ -47,6 +71,11 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            isOnGround = true;
+        }
+
         if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
             Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
